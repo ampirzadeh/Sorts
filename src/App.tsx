d45e3177dfda50e80f20bcb0960f792a9edf9d2d
AmirMohammad Pirzadeh <am.pirzadeh@gmail.com>
2023-10-22 19:27:11 +0100

    Implemented insertion, bubble, and selection sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,108 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  BubbleSort,
+  InsertionSort,
+  SelectionSort,
+  Shuffle,
+  SortingGenerator,
+} from './sorting'
 
 function App() {
+  const [arrayToSort, setArrayToSort] = useState<number[]>([])
+  const [arrSize, setArrSize] = useState(200)
+  const [effectedElementIdx, setEffectedElementIdx] = useState<number[]>([])
+  const [isSorting, setIsSorting] = useState(false)
+
+  useEffect(() => {
+    setArrayToSort(
+      Array.from({ length: arrSize }, (_, i) => i).sort(() => Math.random() - 0.5)
+    )
+  }, [arrSize])
+
+  const initSort = async (generator: SortingGenerator) => {
+    setIsSorting(true)
+    sort(generator)
+  }
+
+  const sort = (generator: SortingGenerator) => {
+    const nextStep = generator.next()
+    if (!nextStep.done) {
+      setArrayToSort([...nextStep.value.nextStep])
+      setEffectedElementIdx([...nextStep.value.effectedElementIndices])
+      setTimeout(() => {
+        sort(generator)
+      }, arrSize / 20)
+    } else {
+      setIsSorting(false)
+    }
+  }
+
   return (
-    <h1
-      className={`${
+    <div
+      className={`flex flex-col h-screen dark:bg-gray-900 text-gray-900 dark:text-gray-200 ${
         process.env.NODE_ENV === 'development' ? 'debug-screens' : ''
-      } text-3xl font-bold underline`}
+      }`}
     >
-      Hello world!
-    </h1>
+      <nav className="flex gap-3 glass top-0 right-0 z-10 p-3">
+        <form className="border-solid border-r border-gray-400 px-3">
+          <input
+            disabled={isSorting}
+            value={arrSize}
+            onChange={(e) => setArrSize(+e.target.value)}
+            type="number"
+            className="bg-gray-100 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 disabled:cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 disabled:dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </form>
+
+        <button
+          disabled={isSorting}
+          className="px-4 py-2 text-white bg-red-500 border-none rounded-md disabled:opacity-50"
+          onClick={() => initSort(Shuffle(arrayToSort))}
+        >
+          Shuffle
+        </button>
+
+        <button
+          disabled={isSorting}
+          className="px-4 py-2 text-white bg-red-500 border-none rounded-md disabled:opacity-50"
+          onClick={() => initSort(InsertionSort(arrayToSort))}
+        >
+          Insertion Sort
+        </button>
+
+        <button
+          disabled={isSorting}
+          className="px-4 py-2 text-white bg-red-500 border-none rounded-md disabled:opacity-50"
+          onClick={() => initSort(BubbleSort(arrayToSort))}
+        >
+          Bubble Sort
+        </button>
+
+        <button
+          disabled={isSorting}
+          className="px-4 py-2 text-white bg-red-500 border-none rounded-md disabled:opacity-50"
+          onClick={() => initSort(SelectionSort(arrayToSort))}
+        >
+          Selection Sort
+        </button>
+      </nav>
+
+      <div className="flex flex-row flex-wrap-reverse h-5/6 overflow-hidden fixed bottom-0 right-0 left-0">
+        {arrayToSort.map((num, i) => (
+          <div
+            key={num}
+            className={`flex-1`}
+            style={{
+              height: `${(num / arrSize) * 100}%`,
+              backgroundColor:
+                effectedElementIdx.indexOf(i) === -1
+                  ? `hsl(0, 100%, ${((num * 0.8) / arrSize) * 100}%)`
+                  : `blue`,
+            }}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/sorting.ts
@@ -0,0 +1,58 @@
+export type StepState = {
+  nextStep: number[]
+  effectedElementIndices: number[]
+}
+
+export type SortingGenerator = Generator<StepState, StepState>
+
+export function* InsertionSort(arr: number[]): SortingGenerator {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j]
+      j = j - 1
+    }
+    arr[j + 1] = key
+
+    yield { nextStep: arr, effectedElementIndices: [i, j] }
+  }
+
+  return { nextStep: arr, effectedElementIndices: [] }
+}
+
+export function* BubbleSort(arr: number[]): SortingGenerator {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++)
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+
+    yield { nextStep: arr, effectedElementIndices: [i, j] }
+  }
+  return { nextStep: arr, effectedElementIndices: [] }
+}
+
+export function* SelectionSort(arr: number[]): SortingGenerator {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min_idx = i
+    for (let j = i + 1; j < arr.length; j++) if (arr[j] < arr[min_idx]) min_idx = j
+    ;[arr[min_idx], arr[i]] = [arr[i], arr[min_idx]]
+    yield { nextStep: arr, effectedElementIndices: [min_idx, i] }
+  }
+
+  return { nextStep: arr, effectedElementIndices: [] }
+}
+
+export function* Shuffle(arr: number[]): SortingGenerator {
+  let currentIndex = arr.length,
+    randomIndex
+
+  while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]]
+    yield { nextStep: arr, effectedElementIndices: [randomIndex, currentIndex] }
+  }
+
+  return { nextStep: arr, effectedElementIndices: [currentIndex] }
+}
