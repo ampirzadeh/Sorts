68a7f06554f20e269276e6d3ce1a503249fbb312
AmirMohammad Pirzadeh <am.pirzadeh@gmail.com>
2023-10-22 19:33:59 +0100

    Updates

--- _nuxt/static/1637398285/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSorts\u002F_nuxt\u002Fstatic\u002F1637398285",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSorts\u002F",assetsPath:"\u002FSorts\u002F_nuxt\u002F",cdnURL:a}}}}(null));
